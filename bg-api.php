e5870da2e5c2a3273dea6589005054110f622a72
Lasse Brenden <lassebrenden@gmail.com>
2021-10-21 19:51:05 +0200

    Fix namespacing

--- bg-api.php
+++ bg-api.php
@@ -19,15 +19,15 @@ setlocale(LC_ALL, 'nb_NO.utf8');
 * Define plugin constants
 */
 
-const BG__v4__IMAGE_PATH = 'https://wp.blgr.app/wp-content/uploads';
-const BG__v4__IMAGE_SIZE = 'large';
-const BG__v4__REST_NAMESPACE = 'bg/v4';
+const BGv4__IMAGE_PATH = 'https://wp.blgr.app/wp-content/uploads';
+const BGv4__IMAGE_SIZE = 'large';
+const BGv4__REST_NAMESPACE = 'bg/v4';
 
 /**
 * Define helper functions
 */
 
-function bg__v4__filter($str) {
+function bgv4__filter($str) {
   $filter = [
     '/&#8211;/' => '–',
     '/&#8212;/' => '–',
@@ -55,7 +55,7 @@ function bg__v4__filter($str) {
   return preg_replace(array_keys($filter), array_values($filter), $str);
 }
 
-function bg__v4__get_author($post_id) {
+function bgv4__get_author($post_id) {
   $post_object = get_post($post_id);
   $author_id = $post_object->post_author;
   $author_firstname = get_the_author_meta('first_name', $author_id);
@@ -64,13 +64,13 @@ function bg__v4__get_author($post_id) {
   return trim("{$author_firstname} {$author_lastname}");
 }
 
-function bg__v4__get_category($post_id) {
+function bgv4__get_category($post_id) {
   $category_object = get_the_category($post_id);
   
   return $category_object[0]->name;
 }
 
-function bg__v4__get_content($post_id) {
+function bgv4__get_content($post_id) {
   $post_object = get_post($post_id);
   $post_content = $post_object->post_content;
   $has_blocks = preg_match('/wp:paragraph/', $post_content);
@@ -79,43 +79,43 @@ function bg__v4__get_content($post_id) {
     $post_content = preg_replace("/\\n/", "\n\n", $post_content);
   }
 
-  return bg__v4__filter(apply_filters('the_content', $post_content));
+  return bgv4__filter(apply_filters('the_content', $post_content));
 }
 
-function bg__v4__get_date($post_id) {
+function bgv4__get_date($post_id) {
   $post_object = get_post($post_id);
   $timestamp = strtotime($post_object->post_date);
 
   return strftime('%-d. %B %Y kl. %H.%M', $timestamp);
 }
 
-function bg__v4__get_image($post_id) {
+function bgv4__get_image($post_id) {
   if (has_post_thumbnail($post_id)) {
     $image_id = get_post_thumbnail_id($post_id);
-    $image_object = wp_get_attachment_image_src($image_id, BG__v4__IMAGE_SIZE);
+    $image_object = wp_get_attachment_image_src($image_id, BGv4__IMAGE_SIZE);
     $image_basename = basename($image_object[0]);
 
-    return BG__v4__IMAGE_PATH . "/{$image_basename}";
+    return BGv4__IMAGE_PATH . "/{$image_basename}";
   }
 
   return false;
 }
 
-function bg__v4__get_slug($post_id) {
+function bgv4__get_slug($post_id) {
   $post_object = get_post($post_id);
   $category_object = get_the_category($post_id);
 
   return "/{$category_object[0]->slug}/{$post_id}/{$post_object->post_name}";
 }
 
-function bg__v4__get_title($post_id) {
+function bgv4__get_title($post_id) {
   $post_object = get_post($post_id);
   $title = $post_object->post_title;
 
-  return bg__v4__filter(apply_filters('the_title', $title));
+  return bgv4__filter(apply_filters('the_title', $title));
 }
 
-function bg__v4__get_next_post($post_id) {
+function bgv4__get_next_post($post_id) {
   global $post;
 
   $post = get_post($post_id);
@@ -125,15 +125,15 @@ function bg__v4__get_next_post($post_id) {
   wp_reset_postdata();
 
   return [
-    'post_category' => bg__v4__get_category($post_id),
+    'post_category' => bgv4__get_category($post_id),
     'post_id' => $post_id,
-    'post_image' => bg__v4__get_image($post_id),
-    'post_slug' => bg__v4__get_slug($post_id),
-    'post_title' => bg__v4__get_title($post_id),
+    'post_image' => bgv4__get_image($post_id),
+    'post_slug' => bgv4__get_slug($post_id),
+    'post_title' => bgv4__get_title($post_id),
   ];
 }
 
-function bg__v4__get_previous_post($post_id) {
+function bgv4__get_previous_post($post_id) {
   global $post;
 
   $post = get_post($post_id);
@@ -143,42 +143,42 @@ function bg__v4__get_previous_post($post_id) {
   wp_reset_postdata();
 
   return [
-    'post_category' => bg__v4__get_category($post_id),
+    'post_category' => bgv4__get_category($post_id),
     'post_id' => $post_id,
-    'post_image' => bg__v4__get_image($post_id),
-    'post_slug' => bg__v4__get_slug($post_id),
-    'post_title' => bg__v4__get_title($post_id),
+    'post_image' => bgv4__get_image($post_id),
+    'post_slug' => bgv4__get_slug($post_id),
+    'post_title' => bgv4__get_title($post_id),
   ];
 }
 
-function bg__v4__render_page($ref) {
+function bgv4__render_page($ref) {
   $post_id = $ref->ID;
   $post_object = get_post($post_id);
   $page_object = [
-    'post_content' => bg__v4__get_content($post_id),
-    'post_date' => bg__v4__get_date($post_id),
+    'post_content' => bgv4__get_content($post_id),
+    'post_date' => bgv4__get_date($post_id),
     'post_id' => $post_id,
-    'post_image' => bg__v4__get_image($post_id),
+    'post_image' => bgv4__get_image($post_id),
     'post_slug' => "/{$post_object->post_name}",
-    'post_title' => bg__v4__get_title($post_id),
+    'post_title' => bgv4__get_title($post_id),
   ];
 
   return $page_object;
 }
 
-function bg__v4__render_post($ref) {
+function bgv4__render_post($ref) {
   $post_id = $ref->ID;
   $post_object = [
-    'post_author' => bg__v4__get_author($post_id),
-    'post_category' => bg__v4__get_category($post_id),
-    'post_content' => bg__v4__get_content($post_id),
-    'post_date' => bg__v4__get_date($post_id),
+    'post_author' => bgv4__get_author($post_id),
+    'post_category' => bgv4__get_category($post_id),
+    'post_content' => bgv4__get_content($post_id),
+    'post_date' => bgv4__get_date($post_id),
     'post_id' => $post_id,
-    'post_image' => bg__v4__get_image($post_id),
-    'post_slug' => bg__v4__get_slug($post_id),
-    'post_title' => bg__v4__get_title($post_id),
-    'next_post' => bg__v4__get_next_post($post_id),
-    'previous_post' => bg__v4__get_previous_post($post_id),
+    'post_image' => bgv4__get_image($post_id),
+    'post_slug' => bgv4__get_slug($post_id),
+    'post_title' => bgv4__get_title($post_id),
+    'next_post' => bgv4__get_next_post($post_id),
+    'previous_post' => bgv4__get_previous_post($post_id),
   ];
 
   return $post_object;
@@ -188,23 +188,23 @@ function bg__v4__render_post($ref) {
 * Define plugin functions
 */
 
-function bg__v4__menus($req) {
+function bgv4__menus($req) {
   $menu_id = $req->get_param('menu_id');
 
   return wp_get_nav_menu_items($menu_id);
 }
 
-function bg__v4__pages($req) {
+function bgv4__pages($req) {
   $pages = get_posts([
     'p' => $req->get_param('page_id') ?: NULL,
     'name' => $req->get_param('page_slug') ?: NULL,
     'post_type' => 'page',
   ]);
 
-  return array_map('bg__v4__render_page', $pages);
+  return array_map('bgv4__render_page', $pages);
 }
 
-function bg__v4__posts($req) {
+function bgv4__posts($req) {
   $posts = get_posts([
     'category_name' => $req->get_param('post_category') ?: NULL,
     'p' => $req->get_param('post_id') ?: NULL,
@@ -216,14 +216,14 @@ function bg__v4__posts($req) {
     's' => $req->get_param('search') ?: NULL,
   ]);
 
-  return array_map('bg__v4__render_post', $posts);
+  return array_map('bgv4__render_post', $posts);
 }
 
 /**
 * Initiate plugin
 */
 
-function bg__v4__register_rest_route($namespace, $route, $fn, $args = []) {
+function bgv4__register_rest_route($namespace, $route, $fn, $args = []) {
   return register_rest_route($namespace, $route, [
     'methods' => 'GET',
     'callback' => $fn,
@@ -231,9 +231,9 @@ function bg__v4__register_rest_route($namespace, $route, $fn, $args = []) {
   ]);
 }
 
-function bg__v4__init() {
+function bgv4__init() {
   // Register menus endpoint
-  bg__v4__register_rest_route(BG__v4__REST_NAMESPACE, '/menus(|/(?P<menu_id>\d+))', 'bg__v4__menus', [
+  bgv4__register_rest_route(BGv4__REST_NAMESPACE, '/menus(|/(?P<menu_id>\d+))', 'bgv4__menus', [
     'menu_id' => [
       'sanitize_callback' => function ($value, $req, $key) {
         return intval($value);
@@ -242,7 +242,7 @@ function bg__v4__init() {
   ]);
 
   // Register pages endpoint
-  bg__v4__register_rest_route(BG__v4__REST_NAMESPACE, '/pages(|/(?P<page_id>\d+))', 'bg__v4__pages', [
+  bgv4__register_rest_route(BGv4__REST_NAMESPACE, '/pages(|/(?P<page_id>\d+))', 'bgv4__pages', [
     'page_id' => [
       'sanitize_callback' => function ($value, $req, $key) {
         return intval($value);
@@ -256,7 +256,7 @@ function bg__v4__init() {
   ]);
 
   // Register posts endpoint
-  bg__v4__register_rest_route(BG__v4__REST_NAMESPACE, '/posts(|/(?P<post_id>\d+))', 'bg__v4__posts', [
+  bgv4__register_rest_route(BGv4__REST_NAMESPACE, '/posts(|/(?P<post_id>\d+))', 'bgv4__posts', [
     'limit' => [
       'sanitize_callback' => function ($value, $req, $key) {
         return intval($value);
@@ -308,4 +308,4 @@ function bg__v4__init() {
   ]);
 }
 
-add_action('rest_api_init', 'bg__v4__init');
+add_action('rest_api_init', 'bgv4__init');
